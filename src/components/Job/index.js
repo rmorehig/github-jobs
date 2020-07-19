6395778fe862ef84e2b62abcaf4d1102e834e322
Rafa Moreno <rmh1516@gmail.com>
2020-07-19 10:36:53 +0200

    add styles to job component

--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,19 +1,39 @@
 import React from 'react'
-import { Logo, Info, MoreInfo, Type, Wrapper } from './styles'
+import {
+  Company,
+  Container,
+  Logo,
+  Info,
+  Location,
+  MoreInfo,
+  Time,
+  Title,
+  Type,
+  Wrapper
+} from './styles'
+import { timeSince } from 'utils/date'
 
 const Job = ({ company, company_logo, created_at, location, title, type }) => {
   return (
     <Wrapper>
       <Logo alt={`${company} logo`} src={company_logo} />
-      <Info>
-        <span>{company}</span>
-        <h3>{title}</h3>
-        <Type>{type}</Type>
-      </Info>
-      <MoreInfo>
-        <span>{location} </span>
-        <time>{created_at}</time>
-      </MoreInfo>
+      <Container>
+        <Info>
+          <Company>{company}</Company>
+          <Title>{title}</Title>
+          <Type>{type}</Type>
+        </Info>
+        <MoreInfo>
+          <Location>
+            <span className="material-icons">public</span>
+            <span>{location} </span>
+          </Location>
+          <Time>
+            <span className="material-icons">schedule</span>
+            <time>{`${timeSince(created_at)} ago`}</time>
+          </Time>
+        </MoreInfo>
+      </Container>
     </Wrapper>
   )
 }
--- src/components/Job/styles.js
+++ src/components/Job/styles.js
@@ -1,13 +1,32 @@
 import styled from 'styled-components'
 
+export const Company = styled.span`
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 14px;
+`
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  margin-left: 16px;
+`
+
 export const Logo = styled.img`
   border-radius: 4px;
   height: 90px;
   object-fit: contain;
   width: 90px;
 `
-export const Info = styled.div`
-  flex: 1;
+
+export const Info = styled.div``
+
+export const Location = styled.div`
+  align-items: center;
+  display: flex;
+  .material-icons {
+    margin-right: 7.5px;
+  }
 `
 
 export const MoreInfo = styled.div`
@@ -16,8 +35,18 @@ export const MoreInfo = styled.div`
   font-weight: 500;
   font-size: 12px;
   line-height: 14px;
+  margin-top: 24px;
 `
 
+export const Time = styled(Location)`
+  margin-left: 28px;
+`
+export const Title = styled.h3`
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  margin: 8px 0 14px;
+`
 export const Type = styled.span`
   border: 1px solid #334680;
   border-radius: 4px;
@@ -32,5 +61,6 @@ export const Wrapper = styled.li`
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
   border-radius: 4px;
   display: flex;
+  margin-bottom: 23px;
   padding: 12px;
 `
